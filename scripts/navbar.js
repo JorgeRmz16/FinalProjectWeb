b596592d87562993e6f57e836587f6f88f93ee06
JorgeRmz <jorgermzc16@example.com>
2024-08-17 22:07:55 -0600

    navbar modification

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     fetch('../pages/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar').innerHTML = data;
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
 
-        const cart = JSON.parse(localStorage.getItem('cart')) || [];
-        const count = cart.reduce((total, item) => total + item.quantity, 0);
-        document.getElementById('cart-count').textContent = count;
-    })
-    .catch(error => console.error('Error al cargar la navbar:', error));
-    const cartCountElement = document.getElementById('cart-count');
-
+            const cartCountElement = document.getElementById('cart-count');
+            if (cartCountElement) {
+                const cart = JSON.parse(localStorage.getItem('cart')) || [];
+                const count = cart.reduce((total, item) => total + item.quantity, 0);
+                cartCountElement.textContent = count;
+            } else {
+                console.error('El elemento con ID "cart-count" no se encontró en la navbar.');
+            }
+        })
+        .catch(error => console.error('Error al cargar la navbar:', error));
 });
